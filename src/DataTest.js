d5a8a557dfbca99a4d521348772ef836e482bc46
jerry <195658469@qq.com>
2020-01-12 12:23:10 +0800

    flush

--- src/DataTest.js
+++ src/DataTest.js
@@ -1,19 +1,13 @@
-// const Data = [];
-// for (let i = 1; i <= 100000; i += 1) {
-//   const rows = [];
-//   for (let j = 1; j <= 80; j++) {
-//     rows.push({
-//       text: `行: ${i} 列:${j}`,
-//     });
-//   }
-//   Data.push(rows);
-// }
-const Data = new Array(1000).fill('').map( (row, ri) => {
-  return new Array(50).fill('').map((col, ci) => {
-    return {
-      text: `行: ${ri} 列:${ci}`
-    }
-  })
-});
+const Data = [];
+
+for (let i = 1; i <= 1000; i += 1) {
+  const rows = [];
+  for (let j = 1; j <= 80; j++) {
+    rows.push({
+      text: `行: ${i} 列:${j}`,
+    });
+  }
+  Data.push(rows);
+}
 
 export { Data };
