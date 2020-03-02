97f6ccdcf073aa7944f452ff717c492dc2dc2c4b
jerry <195658469@qq.com>
2020-03-02 10:36:21 +0800

    flush

--- src/DataTest.js
+++ src/DataTest.js
@@ -9,12 +9,6 @@ for (let i = 0; i < 1000; i += 1) {
     const item = Utils.mergeDeep(Cells.getDefaultAttr(), {
       text: `行: ${i + 1} 列:${j + 1}`,
     });
-    if (i === 3 && j === 3) {
-      item.borderAttr.top.display = true;
-      item.borderAttr.left.display = true;
-      item.borderAttr.bottom.display = true;
-      item.borderAttr.right.display = true;
-    }
     rows.push(item);
   }
   Data.push(rows);
