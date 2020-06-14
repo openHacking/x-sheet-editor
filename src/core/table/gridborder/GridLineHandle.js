a4d77d1be87e250134a92bbac5de7fb842c14ce2
jerry <jerry@jerrydeMacBook-Pro.local>
2020-06-14 23:46:02 +0800

    flush

--- src/core/table/gridborder/GridLineHandle.js
+++ src/core/table/gridborder/GridLineHandle.js
@@ -1,4 +1,4 @@
-import { ALIGN, TEXT_WRAP } from '../../../canvas/Font';
+import { ALIGN, TEXT_WRAP, TEXT_DIRECTION } from '../../../canvas/Font';
 import { Utils } from '../../../utils/Utils';
 
 /**
@@ -40,17 +40,47 @@ class GridLineHandle {
     let checkedRight = true;
     let checkedLeft = true;
 
-    if (master) {
+    // 检查master单元格是否越界
+    if (master && !Utils.isBlank(master.text)) {
       const { fontAttr } = master;
-      const { textWrap } = fontAttr;
-      if (textWrap === TEXT_WRAP.OVER_FLOW) {
-        const { align } = fontAttr;
-        const maxWidth = cols.getWidth(ci);
-        if (align === ALIGN.left || align === ALIGN.center) {
-          const width = this.getCellContentWidth(master, ci);
-          if (width > maxWidth) {
-            if (next === null || Utils.isBlank(next.text)) checkedLeft = false;
-          }
+      const { direction } = fontAttr;
+      let checked = true;
+      // 检查文本的绘制方向
+      // 区别对待旋转文本
+      if (direction === TEXT_DIRECTION.ANGLE) {
+        const { angle, textWrap } = fontAttr;
+        if (angle === 90 || angle === -90) {
+          checked = false;
+        }
+        if (textWrap === TEXT_WRAP.TRUNCATE) {
+          checked = false;
+        }
+      } else {
+        // 跳过裁剪类型不是overflow
+        // 类型的单元格
+        // eslint-disable-next-line no-lonely-if
+        const { textWrap } = fontAttr;
+        if (textWrap !== TEXT_WRAP.OVER_FLOW) {
+          checked = false;
+        }
+      }
+      // 跳过对齐方式不是left和center
+      // 类型的单元格
+      const { align } = fontAttr;
+      const maxWidth = cols.getWidth(ci);
+      if (align !== ALIGN.left && align !== ALIGN.center) {
+        checked = false;
+      }
+      // 单元格符合检查条件
+      // 则检查宽度是否越界
+      if (checked) {
+        // 检查当前单元格的内容
+        // 宽度是否越界
+        const width = this.getCellContentWidth(master, ci);
+        if (width > maxWidth) {
+          // 只有next单元格是空时
+          // 才允许不绘制边框
+          if (next === null || Utils.isBlank(next.text)) checkedLeft = false;
         }
       }
     }
@@ -58,23 +88,40 @@ class GridLineHandle {
     // 检查左边是否越界
     let leftWidth = cols.getWidth(ci - 1) + cols.getWidth(ci);
     for (let i = ci - 1; i >= 0; i -= 1, leftWidth += cols.getWidth(i)) {
+      // 过滤掉空单元格
       const cell = cells.getCell(ri, i);
       if (cell === null) continue;
       const { text } = cell;
       if (Utils.isBlank(text)) continue;
+      // 检查文本的绘制方向
+      // 区别对待旋转文本
       const { fontAttr } = cell;
-      const { textWrap } = fontAttr;
-      if (textWrap === TEXT_WRAP.OVER_FLOW) {
+      const { direction } = fontAttr;
+      if (direction === TEXT_DIRECTION.ANGLE) {
+        const { angle, textWrap } = fontAttr;
+        if (angle === 90 || angle === -90) { break; }
+        if (textWrap === TEXT_WRAP.TRUNCATE) { break; }
+      } else {
+        // 跳过裁剪类型不是overflow
+        // 类型的单元格
+        const { textWrap } = fontAttr;
+        if (textWrap !== TEXT_WRAP.OVER_FLOW) { break; }
+        // 跳过对齐方式不是left和center
+        // 类型的单元格
         const { align } = fontAttr;
-        if (align === ALIGN.left || align === ALIGN.center) {
-          const width = this.getCellContentWidth(cell, i);
-          if (width > leftWidth) {
-            const masterBlank = master === null || Utils.isBlank(master.text);
-            const nextBlank = next === null || Utils.isBlank(next.text);
-            if (masterBlank && nextBlank) {
-              checkedLeft = false;
-            }
-          }
+        if (align !== ALIGN.left && align !== ALIGN.center) { break; }
+      }
+      // 检查当前单元格的内容
+      // 宽度是否越界
+      const width = this.getCellContentWidth(cell, i);
+      if (width > leftWidth) {
+        // 只有master单元格和
+        // next单元格都是空时
+        // 才允许不绘制边框
+        const masterBlank = master === null || Utils.isBlank(master.text);
+        const nextBlank = next === null || Utils.isBlank(next.text);
+        if (masterBlank && nextBlank) {
+          checkedLeft = false;
         }
       }
       break;
@@ -83,21 +130,37 @@ class GridLineHandle {
     // 检查右边是否越界
     let rightWidth = cols.getWidth(ci + 1);
     for (let j = ci + 1; j <= len; j += 1, rightWidth += cols.getWidth(j)) {
+      // 过滤掉空单元格
       const cell = cells.getCell(ri, j);
       if (cell === null) continue;
       const { text } = cell;
       if (Utils.isBlank(text)) continue;
+      // 检查文本的绘制方向
+      // 区别对待旋转文本
       const { fontAttr } = cell;
-      const { align } = fontAttr;
-      const { textWrap } = fontAttr;
-      if (textWrap === TEXT_WRAP.OVER_FLOW) {
-        if (align === ALIGN.right || align === ALIGN.center) {
-          const width = this.getCellContentWidth(cell, j);
-          if (width > rightWidth) {
-            if (master === null || Utils.isBlank(master.text)) {
-              checkedRight = false;
-            }
-          }
+      const { direction } = fontAttr;
+      if (direction === TEXT_DIRECTION.ANGLE) {
+        const { angle, textWrap } = fontAttr;
+        if (angle === 90 || angle === -90) { break; }
+        if (textWrap === TEXT_WRAP.TRUNCATE) { break; }
+      } else {
+        // 跳过裁剪类型不是overflow
+        // 类型的单元格
+        const { textWrap } = fontAttr;
+        if (textWrap !== TEXT_WRAP.OVER_FLOW) { break; }
+        // 跳过对齐方式不是right和center
+        // 类型的单元格
+        const { align } = fontAttr;
+        if (align !== ALIGN.right && align !== ALIGN.center) { break; }
+      }
+      // 检查当前单元格的内容
+      // 宽度是否越界
+      const width = this.getCellContentWidth(cell, j);
+      if (width > rightWidth) {
+        // 只有master单元格为
+        // 空时才允许不绘制边框
+        if (master === null || Utils.isBlank(master.text)) {
+          checkedRight = false;
         }
       }
       break;
