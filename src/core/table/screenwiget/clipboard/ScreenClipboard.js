e94a8dcc54286f77bccb4a9073deb9cd72323d85
jerry_me <195658469@qq.com>
2020-08-24 18:26:15 +0800

    flush

--- src/core/table/screenwiget/clipboard/ScreenClipboard.js
+++
@@ -1,9 +0,0 @@
-import { ScreenWidget } from '../../screen/ScreenWidget';
-
-class ScreenClipboard extends ScreenWidget {
-  constructor(screen, option) {
-    super(screen);
-  }
-}
-
-export { ScreenClipboard };
