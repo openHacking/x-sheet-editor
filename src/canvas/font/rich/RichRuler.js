093bfffb7dbb152559d19971a630b58798e0fe99
jerry <195658469@qq.com>
2021-05-24 07:55:41 +0800

    flush

--- src/canvas/font/rich/RichRuler.js
+++ src/canvas/font/rich/RichRuler.js
@@ -2,6 +2,13 @@ import { BaseRuler } from '../BaseRuler';
 
 class RichRuler extends BaseRuler {
 
+  constructor({
+    draw, rich,
+  }) {
+    super({ draw });
+    this.rich = rich;
+  }
+
 }
 
 export {
--- src/canvas/font/rich/ruler/RichHorizonRuler.js
+++ src/canvas/font/rich/ruler/RichHorizonRuler.js
@@ -1,6 +1,40 @@
 import { RichRuler } from '../RichRuler';
+import { BaseRuler } from '../../BaseRuler';
 
 class RichHorizonRuler extends RichRuler {
+
+  constructor({
+    draw, rich, size, rect, overflow, align, textWrap, lineHeight = 8, padding,
+  }) {
+    super({
+      rich, draw, align, padding,
+    });
+
+    this.overflow = overflow;
+    this.textWrap = textWrap;
+    this.size = size;
+    this.rect = rect;
+    this.lineHeight = lineHeight;
+    this.align = align;
+    this.padding = padding;
+    this.used = BaseRuler.USED.DEFAULT_INI;
+
+    // 裁剪文本
+    this.truncateText = []; // [[x,y],[x,y],....]
+    this.truncateTextWidth = 0;
+    this.truncateTextHeight = 0;
+    this.truncateTextAscent = 0;
+
+    // 溢出文本
+    this.overflowText = []; // [[x,y],[x,y],....]
+    this.overflowTextWidth = 0;
+    this.overflowTextHeight = 0;
+    this.overflowTextAscent = 0;
+
+    // 自动换行文本
+    this.textWrapTextArray = []; // [[x,y],[x,y],....]
+    this.textWrapTextHeight = 0;
+  }
 
 }
 
