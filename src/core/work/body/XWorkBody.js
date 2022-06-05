2aefeafebb391ad6276142f60a39735ba2b80817
openHacking <openHacking@126.com>
2022-06-05 21:14:04 +0800

    add api addsheetbyconfig

--- src/core/work/body/XWorkBody.js
+++ src/core/work/body/XWorkBody.js
@@ -382,6 +382,17 @@ class XWorkBody extends Widget {
   }
 
   /**
+   * 从数据添加一个新的 tab sheet
+   * @param tab
+   * @param sheet
+   */
+  addTabSheetByConfig(sheetConfig) {
+    const tab = new XWorkTab(sheetConfig.name);
+    const sheet = new XWorkSheet(tab, sheetConfig);
+    this.addTabSheet(tab, sheet);
+  }
+
+  /**
    * 删除指定索引的sheet
    * @param index
    */
